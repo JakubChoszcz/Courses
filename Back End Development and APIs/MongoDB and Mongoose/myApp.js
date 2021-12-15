96a5bd4c95ef2e9f0d64e850746e415b5fc65418
Choszcz Jakub <jakub.choszcz@gmail.com>
2021-12-15 17:08:33 +0100

    Add the two first projects "Back End Development and APIs"

---
+++ Back End Development and APIs/MongoDB and Mongoose/myApp.js
@@ -0,0 +1,152 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Create a Model
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  name: { type: String, require: true },
+  age: Number,
+  favoriteFoods: [String] 
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+// Create and Save a Record of a Model
+const createAndSavePerson = (done) => {
+  const jakubChoszcz = new Person({ name: 'Jakub', age: 20, favoriteFoods: ['', 'chocolate', 'dumplings']});
+
+  jakubChoszcz.save((err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    done(null, data);
+  })
+};
+
+// Create Many Records with model.create()
+const arrayOfPeople = [
+  { name: 'Konrad', age: 20, favoriteFood: ['quesadillas', 'avocado', 'kebab'] },
+  { name: 'Katarzyna', age: 21, favoriteFood: [] },
+  { name: 'Krzysztof', age: 19, favoriteFood: ['pizza', 'fries'] }
+];
+
+const createManyPeople = (arrayOfPeople, done) => {
+  Person.create(arrayOfPeople, (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    done(null, data);
+  })
+};
+
+// Use model.find() to Search Your Database
+const findPeopleByName = (personName, done) => {
+  Person.find({ name: personName }, (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    done(null, data);
+  })
+};
+
+// Use model.findOne() to Return a Single Matching Document from Your Database
+const findOneByFood = (food, done) => {
+  Person.findOne({ favoriteFoods: food }, (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    done(null, data);
+  })
+};
+
+// Use model.findById() to Search Your Database By _id
+const findPersonById = (personId, done) => {
+  Person.findById({ _id: personId }, (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    done(null, data);
+  })
+};
+
+// Perform Classic Updates by Running Find, Edit, then Save
+const findEditThenSave = (personId, done) => {
+  const foodToAdd = "hamburger";
+
+  Person.findById(personId, (err, person) => {
+    if (err) return console.log(err);
+
+    person.favoriteFoods.push(foodToAdd);
+
+    person.save((err, updatedPerson) => {
+      if (err) return console.log(err);
+
+      done(null, updatedPerson);
+    })
+  })
+};
+
+// Perform New Updates on a Document Using model.findOneAndUpdate()
+const findAndUpdate = (personName, done) => {
+  const ageToSet = 20;
+
+  Person.findOneAndUpdate({ name: personName }, { age: ageToSet }, { new: true }, (err, data) => {
+    if (err) return console.log(err);
+
+    done(null, data);
+  })
+};
+
+// Delete One Document Using model.findByIdAndRemove
+const removeById = (personId, done) => {
+  Person.findByIdAndRemove(personId, (err, data) => {
+    if (err) return console.log(err);
+
+    done(null, data);
+  })
+};
+
+// Delete Many Documents with model.remove()
+const removeManyPeople = (done) => {
+  const nameToRemove = "Mary";
+
+  Person.deleteMany({ name: nameToRemove }, (err, data) => {
+    if(err) return console.log(err);
+    done(null, data);
+  })
+};
+
+// Chain Search Query Helpers to Narrow Search Results
+const queryChain = (done) => {
+  const foodToSearch = "burrito";
+
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: "asc" })
+    .limit(2)
+    .select("-age")
+    .exec((err, data) => {
+      if (err) return console.log(err);
+      done(null, data);
+    })
+};
+
+/** **Well Done !!**
+/* You completed these challenges, let's go celebrate !
+ */
+
+//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
+
+exports.PersonModel = Person;
+exports.createAndSavePerson = createAndSavePerson;
+exports.findPeopleByName = findPeopleByName;
+exports.findOneByFood = findOneByFood;
+exports.findPersonById = findPersonById;
+exports.findEditThenSave = findEditThenSave;
+exports.findAndUpdate = findAndUpdate;
+exports.createManyPeople = createManyPeople;
+exports.removeById = removeById;
+exports.removeManyPeople = removeManyPeople;
+exports.queryChain = queryChain;
