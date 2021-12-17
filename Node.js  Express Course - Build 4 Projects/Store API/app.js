c98f39b06976506284729922f8a31cb1a0828e67
Choszcz Jakub <jakub.choszcz@gmail.com>
2021-12-17 14:48:00 +0100

    Add "Store API" project"

---
+++ Node.js  Express Course - Build 4 Projects/Store API/app.js
@@ -0,0 +1,42 @@
+console.clear();
+
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const connectDB = require('./db/connect');
+
+const productsRouter = require('./routes/products');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+// middlewares
+app.use(express.json());
+
+// rootes
+app.get('/', (req, res) => {
+    res.send("<h1>Store API</h1><a href='/api/v1/products'>products route</a>")
+});
+
+app.use('/api/v1/products', productsRouter);
+
+// products route
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const start = async () => {
+    try {
+        // connectDB
+        await connectDB(process.env.MONGO_URI);
+
+        app.listen(port, console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+start();
