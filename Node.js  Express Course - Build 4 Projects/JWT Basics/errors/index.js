b4854965871364a7304adb01287cb9ec4b9c0519
JakubChoszcz <jakub.choszcz@gmail.com>
2021-12-27 14:17:07 +0100

    Add "JWT Basics" project

---
+++ Node.js  Express Course - Build 4 Projects/JWT Basics/errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require('./custom-error');
+const BadRequest = require('./bad-request');
+const UnauthendicatedError = require('./unauthenticated');
+
+module.exports = {
+    CustomAPIError,
+    BadRequest,
+    UnauthendicatedError
+};
