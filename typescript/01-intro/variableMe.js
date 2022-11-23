a3b34fa803a13abc2a6e0d4e5d646da4e1d6c2e0
JakubChoszcz <jakub.choszcz@gmail.com>
2022-11-23 22:53:51 +0100

    Add ts primary variables part

---
+++ typescript/01-intro/variableMe.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+var greetings = "Hello Jakub";
+var mynum = 6;
+// mynum.toUpperCase()
+greetings.toLowerCase();
+console.log(greetings);
+// number
+var userId = 334455.66;
+userId.toFixed();
+// userId = "Jakub"
+// boolean
+var isLoggedIn = false;
---
+++ typescript/01-intro/variableMe.ts
@@ -0,0 +1,20 @@
+let greetings: string = "Hello Jakub"
+
+let mynum = 6
+
+// mynum.toUpperCase()
+
+greetings.toLowerCase()
+
+console.log(greetings);
+
+// number
+let userId = 334455.66
+
+userId.toFixed()
+// userId = "Jakub"
+
+// boolean
+let isLoggedIn: boolean = false
+
+export {}
