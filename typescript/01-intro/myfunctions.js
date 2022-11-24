376fb4ae9a0e3b9e9de78a4a9a05463e1afdea48
JakubChoszcz <jakub.choszcz@gmail.com>
2022-11-24 09:18:25 +0100

    Add primary ts functions

---
+++ typescript/01-intro/myfunctions.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+function addTwo(num) {
+    // return num + 2
+    return "hello";
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPadin) { }
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+var value = addTwo(5);
+getUpper("string");
+signUpUser("name", "email@gmail.com", true);
+loginUser("name", "email@gmail.com");
---
+++ typescript/01-intro/myfunctions.ts
@@ -0,0 +1,19 @@
+function addTwo(num: number) {
+    // return num + 2
+    return "hello"
+}
+
+function getUpper(val: string) {
+    return val.toUpperCase()
+}
+
+function signUpUser(name: string, email: string, isPadin: boolean) {}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+
+let value = addTwo(5)
+getUpper("string")
+signUpUser("name", "email@gmail.com", true)
+loginUser("name", "email@gmail.com")
+
+export {}
