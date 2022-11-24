9248e84fac5489210779fa06ae10fb2edc5427db
JakubChoszcz <jakub.choszcz@gmail.com>
2022-11-24 13:54:10 +0100

    Add ts type aliasses

--- typescript/01-intro/myObjects.js
+++ typescript/01-intro/myObjects.js
@@ -1,16 +1,15 @@
 "use strict";
+// const User = {
+//     name: "Jakub",
+//     email: "hitesh@lco.dev",
+//     isActive: true
+// }
 exports.__esModule = true;
-var User = {
-    name: "Jakub",
-    email: "hitesh@lco.dev",
-    isActive: true
-};
-function createUser(_a) {
-    var string = _a.name, boolean = _a.isPaid;
+function createUser(user) {
+    return {
+        name: user.name,
+        email: user.email,
+        isActive: user.isActive
+    };
 }
-var newUser = { name: "Jakub", isPaid: false };
-createUser(newUser);
-function createCourse(_a) {
-    var string = _a.name, _b = _a.price, number = _b === void 0 ? 0 : _b;
-    return { name: 'typescript', price: 399 };
-}
+createUser({ name: "Jakub", email: "", isActive: true });
--- typescript/01-intro/myObjects.ts
+++ typescript/01-intro/myObjects.ts
@@ -1,17 +1,35 @@
-const User = {
-    name: "Jakub",
-    email: "hitesh@lco.dev",
-    isActive: true
+// const User = {
+//     name: "Jakub",
+//     email: "hitesh@lco.dev",
+//     isActive: true
+// }
+
+// function createUser({name: string, isPaid: boolean}) {}
+
+// let newUser = {name: "Jakub", isPaid: false}
+
+// createUser(newUser)
+
+// function createCourse({name: string, price: number = 0}):{name: string, price: number}{
+//     return {name: 'typescript', price: 399}
+// }
+
+type bool = boolean
+
+type User = {
+    name: string;
+    email: string;
+    isActive: bool
 }
 
-function createUser({name: string, isPaid: boolean}) {}
-
-let newUser = {name: "Jakub", isPaid: false}
-
-createUser(newUser)
-
-function createCourse({name: string, price: number = 0}):{name: string, price: number}{
-    return {name: 'typescript', price: 399}
+function createUser(user: User): User {
+    return {
+        name: user.name,
+        email: user.email,
+        isActive: user.isActive
+    }
 }
+
+createUser({name: "Jakub", email: "", isActive: true})
 
 export {}
