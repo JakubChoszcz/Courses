ce521df4d9842c1102ef7a3b581ad7fb3ae3e682
JakubChoszcz <jakub.choszcz@gmail.com>
2022-11-24 09:58:20 +0100

    Add better way to write ts fnc

--- typescript/01-intro/myfunctions.js
+++ typescript/01-intro/myfunctions.js
@@ -1,8 +1,7 @@
 "use strict";
 exports.__esModule = true;
 function addTwo(num) {
-    // return num + 2
-    return "hello";
+    return num + 2;
 }
 function getUpper(val) {
     return val.toUpperCase();
@@ -15,3 +14,23 @@ var value = addTwo(5);
 getUpper("string");
 signUpUser("name", "email@gmail.com", true);
 loginUser("name", "email@gmail.com");
+// function getValue(myVal: number): boolean | string {
+//     if (myVal > 5) {
+//         return true
+//     }
+//     return "200 OK"
+// }
+var getHello = function (s) {
+    return "Hello";
+};
+var heroes = ["thor", "spiderman", "ironman"];
+// const heroes = [1, 2, 3]
+heroes.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+function handleError(errmsg) {
+    throw console.log(errmsg);
+}
--- typescript/01-intro/myfunctions.ts
+++ typescript/01-intro/myfunctions.ts
@@ -1,6 +1,5 @@
-function addTwo(num: number) {
-    // return num + 2
-    return "hello"
+function addTwo(num: number): number {
+    return num + 2
 }
 
 function getUpper(val: string) {
@@ -15,5 +14,32 @@ let value = addTwo(5)
 getUpper("string")
 signUpUser("name", "email@gmail.com", true)
 loginUser("name", "email@gmail.com")
+
+// function getValue(myVal: number): boolean | string {
+//     if (myVal > 5) {
+//         return true
+//     }
+
+//     return "200 OK"
+// }
+
+const getHello = (s: string): string => {
+    return "Hello"
+}
+
+const heroes = ["thor", "spiderman", "ironman"]
+// const heroes = [1, 2, 3]
+
+heroes.map((hero: string): string => {
+    return `hero is ${hero}`
+})
+
+function consoleError(errmsg: string): void {
+    console.log(errmsg);
+}
+
+function handleError(errmsg: string): never {
+    throw console.log(errmsg);
+}
 
 export {}
