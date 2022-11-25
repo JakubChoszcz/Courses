99e44efd50e7fbfef3d26d5001c56ad6fd8d317b
JakubChoszcz <jakub.choszcz@gmail.com>
2022-11-25 12:34:25 +0100

    Add ts arrays

---
+++ typescript/01-intro/myArray.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+var superHeros = [];
+// const heroPower: number[] = []
+var heroPower = [];
+var AllUsers = [];
+var MLModels = [
+    [255, 255, 255],
+    [128, 128, 128],
+];
+superHeros.push("spierdman");
+heroPower.push(2);
+AllUsers.push({ name: "Jakub", isActive: true });
---
+++ typescript/01-intro/myArray.ts
@@ -0,0 +1,22 @@
+const superHeros: string[] = []
+// const heroPower: number[] = []
+const heroPower: Array<number> = []
+
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const AllUsers: User[] = []
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    [128, 128, 128],
+]
+
+superHeros.push("spierdman")
+heroPower.push(2)
+
+AllUsers.push({name: "Jakub", isActive: true})
+
+export {}
