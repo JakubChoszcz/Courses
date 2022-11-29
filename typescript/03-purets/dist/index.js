14915918e227e922b1cb9f853a52913fddf74493
JakubChoszcz <jakub.choszcz@gmail.com>
2022-11-29 15:45:38 +0100

    Add ts get and set methods

--- typescript/03-purets/dist/index.js
+++ typescript/03-purets/dist/index.js
@@ -15,10 +15,27 @@ class User {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
         this.city = "Warsaw";
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEamil() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 const jakub = new User("jc@jc.jc", "Jakub");
 // jakub.city = "Warsaw"
 // jakub.name
 // jakub.getCity();
+// jakub.deleteToken();
--- typescript/03-purets/src/index.ts
+++ typescript/03-purets/src/index.ts
@@ -14,12 +14,32 @@
 // }
 
 class User {
+    private _courseCount = 1
     readonly city: string = "Warsaw"
     constructor(
         public email: string, 
         public name: string,
-        // private userId: string
         ) {
+    }
+
+    private deleteToken() {
+        console.log("Token deleted");
+    }
+
+    get getAppleEamil(): string {
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1")
+        }
+
+        this._courseCount = courseNum
     }
 }
 
@@ -27,3 +47,4 @@ const jakub = new User("jc@jc.jc", "Jakub")
 // jakub.city = "Warsaw"
 // jakub.name
 // jakub.getCity();
+// jakub.deleteToken();
